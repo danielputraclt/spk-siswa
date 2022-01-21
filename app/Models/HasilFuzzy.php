b2026f1826eff8b4cf7bf4f3148f0f89c18f5959
danielputraclt <danielputraclt97@gmail.com>
2022-01-21 12:57:25 +0700

    membuat tampilan dashboard

---
+++ app/Models/HasilFuzzy.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HasilFuzzy extends Model
+{
+    use HasFactory;
+}
